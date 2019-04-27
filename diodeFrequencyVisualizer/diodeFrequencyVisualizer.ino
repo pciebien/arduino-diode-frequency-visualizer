c11c68dc207b76aa38e9d31c91dbdacd6044c497
Patryk Ciebień <cyberitymotion@outlook.com>
2019-04-27 23:47:01 +0200

    Diode logic structure

--- diodeFrequencyVisualizer/diodeFrequencyVisualizer.ino
+++ diodeFrequencyVisualizer/diodeFrequencyVisualizer.ino
@@ -1,15 +1,52 @@
 #include <SoftwareSerial.h>
 
-const byte diode_pin = 8;
+class Diode
+{
+  public:
+    Diode(int pin);
+    void turnOn();
+    void turnOff();
+    void blink(int durationInMs);
+  private:
+    int _pin;
+};
+
+Diode::Diode(int pin)
+{
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);
+  _pin = pin;
+}
+
+void Diode::turnOn()
+{
+  if(digitalRead(_pin) == LOW) {
+    digitalWrite(_pin, HIGH);
+  }
+}
+
+void Diode::turnOff()
+{
+  if(digitalRead(_pin) == HIGH) {
+    digitalWrite(_pin, LOW);
+  }
+}
+
+void Diode::blink(int durationInMs)
+{
+  turnOff();
+  delay(durationInMs);
+  turnOn();
+  delay(durationInMs);
+  turnOff();
+}
+
+Diode redOne(6);
 
 void setup() {
-  pinMode(6, OUTPUT);
-  digitalWrite(6, HIGH);
-  pinMode(diode_pin, OUTPUT);
-  digitalWrite(diode_pin, HIGH);
-  pinMode(9, OUTPUT);
-  digitalWrite(9, HIGH);
+  
 }
 
 void loop() {
+  redOne.blink(20);
 }
